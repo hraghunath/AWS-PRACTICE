b89d2aae086be47725c87facf34ae987dff267f5
hraghunath <hraghunath08@gmail.com>
2023-06-17 07:44:05 +0530

    first commit

---
+++ src/main/java/com/raghunathit/rest/MsgRestController.java
@@ -0,0 +1,19 @@
+package com.raghunathit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgRestController {
+	
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return "Welcome to the RaghunathIt...";
+	}
+	
+	@GetMapping("/greet")
+	public String greetMsg() {
+		return "Good Morning...";
+	}
+
+}
